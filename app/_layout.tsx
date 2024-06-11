555e5df7d6c1a29839cff12908efa54e2215b945
Dan Engel <dengel29@gmail.com>
2024-06-11 09:49:26 +0800

    editor auto-formatting

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,18 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { ApolloProvider } from "@apollo/client";
 import client from "./apollo";
 import { FontSource, useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import "react-native-reanimated";
 import { PortalProvider } from "@gorhom/portal";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync().catch(console.error);
@@ -12,7 +20,7 @@ SplashScreen.preventAutoHideAsync().catch(console.error);
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf') as FontSource,
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf") as FontSource,
   });
 
   useEffect(() => {
